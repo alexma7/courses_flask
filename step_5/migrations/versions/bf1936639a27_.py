18ff02bb1dc03bf2eaef2f46d2aedd60d78ec3ff
Alexma <alexma793@gmail.com>
2022-05-25 00:20:12 +0300

    Обновлена БД для уникальности мейла, доработана форма регистрации с записью в БД

--- step_5/migrations/versions/bf1936639a27_.py
+++ step_5/migrations/versions/bf1936639a27_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 2b5d79788f69
+Revision ID: bf1936639a27
 Revises: 
-Create Date: 2022-05-22 15:00:21.846899
+Create Date: 2022-05-24 23:56:11.073562
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2b5d79788f69'
+revision = 'bf1936639a27'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -23,7 +23,8 @@ def upgrade():
     sa.Column('email', sa.String(), nullable=False),
     sa.Column('password', sa.String(), nullable=False),
     sa.Column('name', sa.String(), nullable=False),
-    sa.PrimaryKeyConstraint('id')
+    sa.PrimaryKeyConstraint('id'),
+    sa.UniqueConstraint('email')
     )
     # ### end Alembic commands ###
 
--- step_5/wsgi.py
+++ step_5/wsgi.py
@@ -1,9 +1,11 @@
 #from crypt import methods
+from enum import unique
 import flask
 import flask_sqlalchemy
 import flask_migrate
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+import sqlalchemy
 
 
 app = flask.Flask(__name__)
@@ -16,7 +18,7 @@ admin = Admin(app)
 
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
 
@@ -48,6 +50,28 @@ def set_auth_view():
         flask.session['is_login_successfull'] = True
         return 'Успешная авторизация'
     return 'Неправильный логин или пароль'
+
+
+@app.route('/register')
+def get_register_view():
+     return flask.render_template('register.html')
+
+
+@app.route('/register' , methods=['POST'])
+def post_register_view():
+    email = flask.request.form.get('email')
+    password = flask.request.form.get('password')
+    name = flask.request.form.get('name')
+    user = UserModel(email=email, password=password, name=name)
+    
+    db.session.add(user)
+    try:
+        db.session.commit()
+    except sqlalchemy.exc.IntegrityError as e:
+        return 'Такой пользователь уже есть'
+    return flask.redirect(flask.url_for('get_auth_view'))
+
+
 
      
 
